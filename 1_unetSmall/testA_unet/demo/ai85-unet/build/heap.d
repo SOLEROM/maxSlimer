acc7719bb82a91ad5212b6b831681be37f85d463
vladSolov <{ID}+{username}@users.noreply.github.com>
2024-08-13 20:10:42 +0300

    auto update

---
+++ 1_unetSmall/testA_unet/demo/ai85-unet/build/heap.d
@@ -0,0 +1,34 @@
+/data/tinyai/maxCNN/test2_unet/demo/ai85-unet/build/heap.o: \
+ /data/tinyai/maxCNN/sdk/Libraries/CMSIS/Device/Maxim/MAX78000/Source/heap.c \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/stdint.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/machine/_default_types.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/features.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/_newlib_version.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/_intsup.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/_stdint.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/errno.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/errno.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/reent.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/_ansi.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/newlib.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/config.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/machine/ieeefp.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/_types.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/machine/_types.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/lock.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/unistd.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/unistd.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/cdefs.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/types.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/machine/endian.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/machine/_endian.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/select.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/_sigset.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/_timeval.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/timespec.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/_timespec.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/sys/sched.h \
+ /data/tinyai/maxCNN/sdk/Tools/SDK_install_root/Tools/GNUTools/10.3/arm-none-eabi/include/machine/types.h
---
+++ 1_unetSmall/testA_unet/demo/ai85-unet/build/startup_max78000.d
@@ -0,0 +1,2 @@
+/data/tinyai/maxCNN/test2_unet/demo/ai85-unet/build/startup_max78000.o: \
+ /data/tinyai/maxCNN/sdk/Libraries/CMSIS/Device/Maxim/MAX78000/Source/GCC/startup_max78000.S
